154d6e441750620923721279d34ce366e9295d62
FalconPilot <fpilot.github@gmail.com>
2019-07-31 01:38:25 +0200

    updated main functioning with basic router

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
-  $pageContent = include(__DIR__.'/pages/index.html');
-  include(__DIR__.'/templates/default.php');
+
+  // renderPage function should only be called once at the same time !
+  function renderPage ($path) {
+    $pageContent = include(__DIR__.'/'.$path);
+    include(__DIR__.'/templates/default.php');
+  }
+
+  // Switch current request URI to actual view
+  $request = $_SERVER['REQUEST_URI'];
+  switch ($request) {
+    default:
+      renderPage('pages/404.html');
+      break;
+  }
 ?>
--- templates/default.php
+++ templates/default.php
@@ -2,8 +2,10 @@
 <html lang="fr">
   <head>
     <meta charset="utf-8" />
+    <link rel="stylesheet" type="text/css" href="/public/main.css" />
   </head>
   <body>
     <?php $pageContent; ?>
+    <?php $_SERVER['REQUEST_URI']; ?>
   </body>
 </html>
